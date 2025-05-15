8061062fd186a4f3c9388d4d8fe8aa4128f20afa
kumkaniasiya <solomonasiya@gmail.com>
2025-05-15 12:29:36 +0200

    new

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,21 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './', // Use relative paths
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
+    assetsInlineLimit: 0, // Force all assets to be separate files
     rollupOptions: {
       output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash][extname]'
+        entryFileNames: '[name]-[hash].js',
+        chunkFileNames: '[name]-[hash].js',
+        assetFileNames: '[name]-[hash][extname]'
       }
+    }
+  },
+  server: {
+    headers: {
+      'Content-Type': 'text/javascript'
     }
   }
 });
